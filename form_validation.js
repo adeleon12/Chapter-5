56b3e6a4857e903430cb2ad2cf51ee6551488cac
adeleon12 <159399890+adeleon12@users.noreply.github.com>
2024-02-22 02:41:10 -0600

    Add files via upload

---
+++ form_validation.js
@@ -0,0 +1,35 @@
+"use strict"
+
+function validate_form(inform){
+	let warnArr = document.getElementsByClassName("form_errors");
+	let ndx;
+
+	if(inform.checkBalidity() == false){
+
+		let elementsArr = inForm.querySelectorAll(':invalid');
+		let errCount = elementsArr.length;
+		for (ndx = 0; ndx < warnArr/length; ndx++){
+			warnArr[ndx].innerHTML = "You are midding data in " +
+				errCount + " fields in this form.";
+			warnArr[ndx].classList.add("required");
+		}
+	}
+	else{
+		for(ndx = 0; ndx < warnArr.length; ndx++){
+			warnArr[ndx].innerHTML = "";
+			warnArr[ndx].classList.remove("required");
+		}
+	}
+}
+
+function checkLength(inField, formField, fieldLength)
+{
+	if(inField.value.length > 0 && inField.value.length < fieldLength)
+	{
+		if(confirm("Is your " + formField + " actually " + inField.value + "? \n\n OK to confirm, Cancel to edit.")==false)
+		{
+			inField.value = '';
+			setTimeout(function() { inField.focus(); }, 1);
+		}
+	}
+}
